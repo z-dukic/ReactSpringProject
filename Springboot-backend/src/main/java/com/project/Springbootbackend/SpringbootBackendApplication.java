3e9a86a71d16563fdf937c13f598f5036c2dcf49
Zoran Đukić <zorandjukic.os@gmail.com>
2022-02-18 22:47:04 +0100

    Starting new react, java project.
    Created springboot project, created database, added database to springboot properties

---
+++ Springboot-backend/src/main/java/com/project/Springbootbackend/SpringbootBackendApplication.java
@@ -0,0 +1,15 @@
+package com.project.Springbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+
+        System.out.println("Hello world");
+	}
+
+}
