0098948a2867bb20c001e8a6c9dd6c910946c415
Zoran Đukić <zorandjukic.os@gmail.com>
2022-02-19 22:14:33 +0100

    Created employee controller and added a couple of data entires to see if it works

---
+++ Springboot-backend/src/main/java/com/project/Springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.project.Springbootbackend.controller;
+
+
+import com.project.Springbootbackend.model.Employee;
+import com.project.Springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+}
