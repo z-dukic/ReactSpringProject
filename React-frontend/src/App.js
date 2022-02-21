2b6e19cca48547143746bb7f24e895646eecff8f
Zoran Đukić <zorandjukic.os@gmail.com>
2022-02-21 11:43:44 +0100

    Created routing with older version.

--- React-frontend/src/App.js
+++ React-frontend/src/App.js
@@ -1,19 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 
 {/* container - bootstrap css class) */ }
+{/* switch - render one component at a time) */ }
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-        <ListEmployeeComponent />
-      </div>
-      <FooterComponent />
+      <Router>
+          <HeaderComponent />
+          <div className="container">
+            <Switch>
+            <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+            <Route path = "/test" exact component = {ListEmployeeComponent}></Route>
+              
+            </Switch>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
