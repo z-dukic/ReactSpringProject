d795ad3df968c9573b502b6d2d37295386a7d18a
Zoran Đukić <zorandjukic.os@gmail.com>
2022-02-21 14:30:02 +0100

    Created update

--- Springboot-backend/src/main/java/com/project/Springbootbackend/controller/EmployeeController.java
+++ Springboot-backend/src/main/java/com/project/Springbootbackend/controller/EmployeeController.java
@@ -45,6 +45,20 @@ public class EmployeeController {
         return ResponseEntity.ok(employee);
     }
 
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+
+    }
+
 }
 
 
