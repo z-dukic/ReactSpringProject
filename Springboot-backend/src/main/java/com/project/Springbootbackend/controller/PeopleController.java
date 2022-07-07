f47a957432c9a7e3bef7ef85cd09e4d37d98adc6
Zoran Đukić <zorandjukic.os@gmail.com>
2022-07-07 15:26:49 +0200

    Update

--- Springboot-backend/src/main/java/com/project/Springbootbackend/controller/PeopleController.java
+++ Springboot-backend/src/main/java/com/project/Springbootbackend/controller/PeopleController.java
@@ -3,6 +3,7 @@ package com.project.Springbootbackend.controller;
 import com.project.Springbootbackend.exception.ResourceNotFoundException;
 import com.project.Springbootbackend.model.People;
 import com.project.Springbootbackend.service.PeopleService;
+import com.project.Springbootbackend.service.PeopleServiceImp;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,25 +14,30 @@ import java.util.Optional;
 @RequestMapping("/api/v1/people")
 public class PeopleController {
 
-    private PeopleService peopleService;
+    private PeopleServiceImp people;
 
-    public PeopleController(PeopleService peopleService) {
-        this.peopleService = peopleService;
+    public PeopleController(PeopleServiceImp people) {
+        this.people = people;
     }
 
     @GetMapping("/name")
     public ResponseEntity<List<People>> findAllByName(@RequestParam String name) {
 
-        return ResponseEntity.ok().body(peopleService.findAllByName(name));
+        return ResponseEntity.ok().body(people.findAllByName(name));
     }
 
-    @GetMapping("/{id}")
+    /* To-do next
+        @GetMapping("/{id}")
     public ResponseEntity<Optional<People>> findById(@PathVariable Integer id) {
 
         People people = peopleService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Person not exist with id :" + id));
 
         return ResponseEntity.ok().body(peopleService.findById(id));
     }
+
+     */
+
+
 
 }
 
